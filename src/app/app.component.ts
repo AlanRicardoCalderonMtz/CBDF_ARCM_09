ed5441a8763126ada26db88369cb88258569f462
AlanRicardoCalderonMtz <AlanRicardoCM@gmail.com>
2023-11-22 08:05:32 -0600

    Ejercicio - 09 Angular - Interpolacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,26 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CBDF_ARCM_09';
+  title = 'Ejercicio 09 - Interpolación';
+  nombre: string = 'Alan Ricardo Calderón Martínez';
+  edad: number = 21;
+  semestre: string = "Septimo semestre";
+  calificaciones: number[] = [9, 10, 10, 9, 8, 10, 9, 10]; 
+  
+  calcular_promedio(){
+    let suma = 0;
+    for(let i=0; i<this.calificaciones.length; i++){
+      suma += this.calificaciones[i];
+    }
+    return suma/this.calificaciones.length;
+  }
+  
+  estadoAlumno(){
+    if(this.calcular_promedio() >= 6){
+      return "Aprobado";
+    }else{
+      return "No aprobado";
+    }
+  }
+
 }
